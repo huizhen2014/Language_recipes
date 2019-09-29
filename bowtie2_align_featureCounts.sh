1246e9de19de0d22144c7350a6058c6e126b1e5b
huizhen2014 <huizhen_2014@163.com>
2019-09-29 18:25:15 +0800

    updated

--- bowtie2_align_featureCounts.sh
+++ bowtie2_align_featureCounts.sh
@@ -2,11 +2,12 @@
 
 #bowtie2-build GCF_000240185.1_ASM24018v2_genomic.fna hs11286_index
 
-bowtie2 -x hs11286_index -1 kp_28_pe_trimmed_1P.fastq.gz -2 kp_28_pe_trimmed_2P.fastq.gz -S kp_28_sangon_mapped.sam
+#bowtie2 -x hs11286_index -1 kp_21_pe_trimmed_1P.fastq.gz -2 kp_21_pe_trimmed_2P.fastq.gz -S kp_21_sangon_mapped.sam
 
-samtools view -bS kp_28_sangon_mapped.sam > kp_28_sangon_mapped.bam
+#samtools view -bS kp_21_sangon_mapped.sam > kp_21_sangon_mapped.bam
 
-samtools sort kp_28_sangon_mapped.bam > kp_28_sangon_mapped_sorted.bam
+##根据比对名称排序
+#samtools sort -n kp_21_sangon_mapped.bam > kp_21_sangon_mapped_sorted.bam
 
 ##-T 线程
 ##-q GTF/GFF 注释文件
@@ -16,6 +17,8 @@ samtools sort kp_28_sangon_mapped.bam > kp_28_sangon_mapped_sorted.bam
 ##-t 默认将一个exon当作一个feature
 ##-o 默认输出
 ##-d -D 最短最长片段
-##-B 指定两端均得比对方才认为有
+##-B 指定两端均得比对方才认为有效
 ##-C 排除chimeric 片段
-featureCounts -p -a HS11286.gtf -g Name -t transcript -d 35 -B -C -o kp_28_sangon.counts kp_28_sangon_mapped_sorted.bam
+##--ignoreDup RNAseq 允许duplication
+##-M count Multi-mapping reads ，目前无推荐
+#featureCounts -p -a HS11286.gtf -g Name -t transcript -d 35 -B -C -o kp_21_sangon.counts kp_21_sangon_mapped_sorted.bam 

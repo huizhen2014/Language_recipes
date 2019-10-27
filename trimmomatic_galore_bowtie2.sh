ca67dd9f47e63a3cdf08a5f04c2c33e07c2a4275
huizhen2014 <huizhen_2014@163.com>
2019-10-27 22:02:26 +0800

    updated

--- trimmomatic_galore_bowtie2.sh
+++ trimmomatic_galore_bowtie2.sh
@@ -1,15 +1,13 @@
-##
-ref=GCF_000786735.1_ASM78673v1_genomic.fna
-read1=LAC_4.raw_1.fastq.gz
-read2=LAC_4.raw_2.fastq.gz
-out=LAC_4
-ref_index=lac_4_index
+##trimmomatic glore bowtie2
+ref=GCF_000240185.1_ASM24018v2_genomic.fna
+read1=kp_21_R1.fastq.gz
+read2=kp_21_R2.fastq.gz
+out=KP_21_trimmo
+index=hs11286_index
+adapter=../Sangon_PE.fa
 
-##PE150
-##去除指定接头
-##minAdapterLength:3
-##keepBothReads:true 
-trimmomatic PE $read1 $read2 -summary ${out}_trimmed.summary -baseout ${out}_pe_trimmed.fastq.gz ILLUMINACLIP:Sangon_adapter.txt:2:30:10:3:true LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:35 
+##trimmomatic 去除指定adapter序列
+trimmomatic PE ${read1} ${read2} -baseout ${out}_trimmed.fastq.gz ILLUMINACLIP:${adapter}:2:30:10:3:keepBothReads LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:35 
 
 ##http://www.bioinformatics.babraham.ac.uk/projects/trim_galore/
 ##--illumina 去除illumina通用adapter序列
@@ -21,28 +19,28 @@ trimmomatic PE $read1 $read2 -summary ${out}_trimmed.summary -baseout ${out}_pe_
 ##-o/--outpur_dir 输出目录，若无自动创建
 ##--basename 输出文件名称:PREFERRED_NAME_trimmed.fq
 ##-j/--cores 运行cores数目
-##-q/--quality 出了adapterremove外，末端保留最小质量阈值
+##-q/--quality 除了adapterremove外，末端保留最小质量阈值
 ##--keep 保留trimmed掉的中间文件
 ##--paired 双端模式，均得满足条件
 ##--fastqc trim后运行fastqc默认分析
-trim_galore --paired -j 4 -q 20 --illumina --stringency 3 --length 35 --trim-n --keep --fastqc --basename ${out}_pe_galore ${out}_pe_trimmed_1P.fastq.gz ${out}_pe_trimmed_2P.fastq.gz
+trim_galore --paired -j 4 -q 20 --illumina --stringency 3 --length 35 --trim-n --keep --fastqc --basename ${out}_galore ${out}_trimmed_1P.fastq.gz ${out}_trimmed_2P.fastq.gz
 
 ##bowtie2 align 
+bowtie2-build ${ref} hs11286_index
 
-#bowtie2-build ${ref} ${ref_index}
+bowtie2 -x hs11286_index -1 ${out}_galore_R1_trimmed_1P_val_1.fq.gz -2 ${out}_galore_R2_trimmed_2P_val_2.fq.gz -S ${out}_sangon_mapped.sam 2> ${out}_bowtie2_map.log
 
-bowtie2 -x ${ref_index} -1 ${out}_pe_galore_R1_trimmed_1P_val_1.fq.gz -2 ${out}_pe_galore_R2_trimmed_2P_val_2.fq.gz -S ${out}_sangon_mapped.sam 2> ${out}_bowtie2_mapping.log
+samtools view -bS  ${out}_sangon_mapped.sam > ${out}_sangon_mapped.bam
 
-samtools view -bS ${out}_sangon_mapped.sam > ${out}_sangon_mapped.bam
-
-##根据比对名称排序
+##根据位置排序
 samtools sort ${out}_sangon_mapped.bam > ${out}_sangon_mapped_sorted.bam
-samtools index -b ${out}_sangon_mapped_sorted.bam 
+samtools index ${out}_sangon_mapped_sorted.bam
 
-rm ${out}_sangon_mapped.sam
+rm ${out}_sangon_mapped.sam 
 rm ${out}_sangon_mapped.bam
 
-##使用Rsubread count
+##使用Rsubread featureCounts
+##-primary
 ##-T 线程
 ##-q GTF/GFF 注释文件
 ##-p paired-end 数据
